a2f31e3cbcb0e908d1e104f13c24b595c4a37865
gaurav_arora <gaurav_arora@newgensoft.com>
2025-01-31 23:33:14 +0530

    ques1 complete

---
+++ task_ques/ques1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.*;
+
+
+
+public class ques1 {
+
+    public static void main (String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number ");
+    int num  =  sc.nextInt();
+    System.out.println(num + "X" + "1" +"="+ (num*1));
+    System.out.println(num + "X" + "2" +"="+ (num*2));
+    System.out.println(num + "X" + "3" +"="+ (num*3));
+    System.out.println(num + "X" + "4" +"="+ (num*4));
+    System.out.println(num + "X" + "5" +"="+ (num*5));
+    System.out.println(num + "X" + "6" +"="+ (num*6));
+    System.out.println(num + "X" + "7" +"="+ (num*7));
+    System.out.println(num + "X" + "8" +"="+ (num*8));
+    System.out.println(num + "X" + "9" +"="+ (num*9));
+    System.out.println(num + "X" + "10" +"="+ (num*10));
+
+
+
+
+    }
+}
