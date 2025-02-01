f356f0e6cb653d80e9409b7b4dbff119e87e743a
gaurav_arora <gaurav_arora@newgensoft.com>
2025-02-01 20:06:23 +0530

    ques3 done

---
+++ task_ques/ques3.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class ques3 {
+
+    public static void main(String args[])
+    {
+//        System.out.println("Enter the value of num 1 ");
+//        Scanner sc = new Scanner(System.in);
+//        int num1  =  sc.nextInt();
+//        System.out.println("Enter the value of num 2 ");
+//        int num2 =  sc.nextInt();
+//        System.out.println("Enter the value of num 3 ");
+//        int num3 =  sc.nextInt();
+//
+//
+//        String  result = (num1 > num2 && num1 > num3) ? num1 +""+"is greatest" :
+//                (num2 >num1 && num2 > num3) ? num2 + " " + "is greatest" :
+//                        num3 + "is greaterst";
+//        System.out.println(result);
+
+//        int a =  10;
+//        System.out.println(++a + a++ + a++);
+//        System.out.println(a);
+
+//        int a  =  20;
+//        System.out.println(--a + a++ + a--);
+//        System.out.println(a);
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the name ");
+//        String name  =  sc.nextLine();
+//        System.out.println("Enter the Salary   ");
+//        int salary =  sc.nextInt();
+//        System.out.println("Enter the age  ");
+//        int age  =  sc.nextInt();
+//
+//        System.out.println(name);
+//        System.out.println(age);
+//        System.out.println(salary);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of num1 ");
+        int num1 =  sc.nextInt();
+        System.out.println("Enter the value of num2  ");
+        int num2 =  sc.nextInt();
+
+        String result = (num1 >num2 ) ? "num1 is greatest" : "num2 is greatest";
+
+        System.out.println(result);
+
+
+
+    }
+
+}
